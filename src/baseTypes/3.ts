13c8125cb183f9bb6bfe71f2b1fe792dba2e6b8a
Serghei Filimonov <filimonov.sergey@gmail.com>
2024-06-30 18:25:33 +0200

    Add Task 3.

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,11 @@
 let some:unknown;
 some = 'Text';
 let str: string;
-str = some;
+
+if (typeof some === 'string') { // Перевірка тип some як string
+    str = some;
+} else {
+    // Обробка випадку, коли some не є строкою
+}
 
 export {};
