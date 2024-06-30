a4619e6fcc5e6eb75a7b87a2f3b57b56c025ba3e
Serghei Filimonov <filimonov.sergey@gmail.com>
2024-06-30 18:49:08 +0200

    Add baseTypes - Task 6.

--- src/baseTypes/6.ts
+++ src/baseTypes/6.ts
@@ -2,7 +2,7 @@
   Як ви вкажете типи для аргументів і значень цих функцій, що повертаються?
 */
 
-function showMessage(message) {
+/*function showMessage(message) {
   console.log(message);
 }
 
@@ -11,6 +11,20 @@ function calc(num1, num2) {
 }
 
 function customError() {
+  throw new Error('Error');
+}
+
+export {};*/
+
+function showMessage(message: string): void {
+  console.log(message);
+}
+
+function calc(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+function customError(): never {
   throw new Error('Error');
 }
 
